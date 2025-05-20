de95d6ca6555f8e7b87e01e16a6ae9c934d67379
Trond Minh <87649787+Mordor828@users.noreply.github.com>
2025-05-20 16:12:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ turtle.speed(0)
 turtle.bgcolor("black")
 
 for i in range(5):
-    for colours in ["red", "cyan", "lime", "lightblue", "magenta", "orange"]:
+    for colours in ["red", "red", "red", "purple", "blue", "purple"]:
         turtle.color(colours)
         turtle.pensize(3)
         turtle.lt(12)
